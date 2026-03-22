64d2fdb65f83da6e8052fcbeac432e3555a0aea8
Jesus Lopez <jesus.lopez.salazar14@gmail.com>
2026-03-22 17:45:31 -0600

    Update Prueba de linkedlist.py

--- Unidad 3 y 4 /ADA4- Implementar Linked List/Prueba de linkedlist.py
+++ Unidad 3 y 4 /ADA4- Implementar Linked List/Prueba de linkedlist.py
@@ -9,11 +9,11 @@ def main():
     mi_lista.insertar_al_inicio("Fresa")
     
     print("Lista original:")
-    mi_lista.mostrar()  # Resultado esperado: Fresa -> Manzana -> Pera -> None
+    mi_lista.mostrar() 
     
     print("\nEliminando 'Manzana':")
     mi_lista.eliminar_nodo("Manzana")
-    mi_lista.mostrar()  # Resultado esperado: Fresa -> Pera -> None
+    mi_lista.mostrar()  
 
 if __name__ == "__main__":
     main()
